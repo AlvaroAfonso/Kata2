571a1f2f5455a1687fb0361af434ae20d036d087
Álvaro Javier Afonso López <alvaroafonso57@gmail.com>
2020-11-04 17:33:04 +0000

    Quinta versión de Kata2

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -11,17 +11,17 @@ import java.util.Map;
 public class Main {
     
     public static void main(String[] args) {
-        
-        Integer data[] = {1,2,3,1,5,6,4,8,9,10,8,12,8,44,9};
+
+        String data[] = {"Hola","que","tal","Hola","bien","y","tal"};
 
         Histogram histo = new Histogram(data);
                 
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String, Integer> histogr = histo.getHistogram();
         
-        Iterator<Map.Entry<Integer, Integer>> entries = histogr.entrySet().iterator();
+        Iterator<Map.Entry<String, Integer>> entries = histogr.entrySet().iterator();
         
         while(entries.hasNext()){
-            Map.Entry<Integer, Integer> entry = entries.next();
+            Map.Entry<String, Integer> entry = entries.next();
             System.out.println(entry.getKey() + "==>" + entry.getValue());
         }
         
